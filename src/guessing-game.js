260f7e1a2c0a4d237fc166beb850707b526ef37d
Radionov_Vitaly <radionovv89@gmail.com>
2022-08-15 13:52:49 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.maxNum = 0;
+        this.minNum = 0;
+        this.number = 409;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.maxNum / 2) + (this.minNum / 2));
+        return this.result;
     }
 
     lower() {
-
+        this.maxNum = this.result;
     }
 
     greater() {
-
+        this.minNum = this.result;
     }
 }
 
